ead3fddfc7320c183ae1c0217bada3895cdff27f
Aditya Puri <74148211+Adityap88@users.noreply.github.com>
2021-02-20 21:56:20 +0530

    Update HomeScreen.java

--- app/src/main/java/com/aditya/atom_project/HomeScreen.java
+++ app/src/main/java/com/aditya/atom_project/HomeScreen.java
@@ -37,7 +37,7 @@ public class HomeScreen extends AppCompatActivity {
             } else {
 
            
-                txtWelcome.setText(R.string.welcome_msg+user.getDisplayName());
+                txtWelcome.setText("Welcome"+user.getDisplayName());
             }
         }
         assert user != null;
